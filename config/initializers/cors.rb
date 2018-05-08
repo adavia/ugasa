bdf1474f5e3c9c2c8b9638ca127f6c6a146327de
adavia <andresi.davia@gmail.com>
2018-05-07 20:45:54 -0500

    Change cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '45.63.106.121'
+    origins '*'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
